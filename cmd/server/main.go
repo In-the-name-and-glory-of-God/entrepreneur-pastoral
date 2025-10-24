c010bfa8a1487af3a899956a05f5883d0856b4e6
Luiz Otavio Utzig <30296841+mr-utzig@users.noreply.github.com>
2025-10-24 00:38:00 -0300

    Setup database connection (#14)
    * feat(2): Create functions to get environment variables
    
    * feat(2): Create config structure and load method
    
    * feat(2): Setup database connection
    
    * feat(2): Setup uber zap for logging
    
    * feat(2): Setup app on main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"fmt"
-
-	_ "github.com/lib/pq"
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/config"
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/database"
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/logger"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	cfg := config.Load()
+
+	log := logger.New(cfg.Application.Env)
+
+	db, err := database.New(cfg.Database)
+	if err != nil {
+		log.Fatal("failed to connect to database", err)
+	}
+	defer db.Close()
+
+	log.Info("database connection established")
+
+	log.Info("starting server")
 }
