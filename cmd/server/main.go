430e6450b85f0b8627201b4aaeb0e3e96d53a5df
Mr. Utzig <lutzig.octavius@gmail.com>
2025-10-23 23:40:27 -0300

    feat(2): Create config structure and load method

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/config"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	config := config.Load()
+
 	fmt.Println("Hello World")
+	fmt.Println(config)
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,80 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/env"
+)
+
+type (
+	Config struct {
+		Database Database
+		Redis    Redis
+		RabbitMQ RabbitMQ
+	}
+
+	Database struct {
+		Host            string
+		Port            int
+		User            string
+		Password        string
+		Name            string
+		Params          string
+		MaxOpenConns    int
+		MaxIdleConns    int
+		ConnMaxLifetime time.Duration
+	}
+
+	Redis struct {
+		Host     string
+		Port     int
+		Password string
+	}
+
+	RabbitMQ struct {
+		Host     string
+		Port     int
+		User     string
+		Password string
+	}
+)
+
+func Load() Config {
+	return Config{
+		Database: Database{
+			Host:            env.GetString("DB_HOST", "localhost"),
+			Port:            env.GetInt("DB_PORT", 5432),
+			User:            env.GetString("DB_USER", "user"),
+			Password:        env.GetString("DB_PASSWORD", "password"),
+			Name:            env.GetString("DB_NAME", "entrepreneur-pastoral"),
+			Params:          env.GetString("DB_PARAMS", "sslmode=disable"),
+			MaxOpenConns:    env.GetInt("DB_MAX_OPEN_CONNS", 25),
+			MaxIdleConns:    env.GetInt("DB_MAX_IDLE_CONNS", 25),
+			ConnMaxLifetime: env.GetDuration("DB_CONN_MAX_LIFETIME", 5*time.Minute),
+		},
+		Redis: Redis{
+			Host:     env.GetString("REDIS_HOST", "localhost"),
+			Port:     env.GetInt("REDIS_PORT", 6379),
+			Password: env.GetString("REDIS_PASSWORD", ""),
+		},
+		RabbitMQ: RabbitMQ{
+			Host:     env.GetString("RABBITMQ_HOST", "localhost"),
+			Port:     env.GetInt("RABBITMQ_PORT", 5672),
+			User:     env.GetString("RABBITMQ_USER", "guest"),
+			Password: env.GetString("RABBITMQ_PASSWORD", "guest"),
+		},
+	}
+}
+
+func (d Database) DSN() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?%s", d.User, d.Password, d.Host, d.Port, d.Name, d.Params)
+}
+
+func (r Redis) Addr() string {
+	return fmt.Sprintf("%s:%d", r.Host, r.Port)
+}
+
+func (r RabbitMQ) DSN() string {
+	return fmt.Sprintf("amqp://%s:%s@%s:%d/", r.User, r.Password, r.Host, r.Port)
+}
