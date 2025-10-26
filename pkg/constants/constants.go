645eaaf6e4200eeaaacbef2f86d5683ba3fee8f6
Mr. Utzig <lutzig.octavius@gmail.com>
2025-10-26 01:58:39 -0300

    feat: Create role constants

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -2,3 +2,11 @@ package constants
 
 const DEVELOPMENT = "development"
 const PRODUCTION = "production"
+
+const (
+	ROLE_ADMIN int16 = iota
+	ROLE_MANAGER
+	ROLE_ASSISTENT
+	ROLE_ENTREPRENEUR
+	ROLE_USER
+)
