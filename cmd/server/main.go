a5f73a5ea12e167ace580f5a27387643830f7ead
Mr. Utzig <lutzig.octavius@gmail.com>
2025-11-09 23:39:39 -0300

    feat: Add graceful shutdown + router + orchestrator

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,32 +1,93 @@
 package main
 
 import (
-	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/cache"
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/cmd/server/orchestrator"
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/cmd/server/router"
 	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/config"
 	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/database"
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/helper/auth"
 	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/logger"
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/storage"
+	"go.uber.org/zap"
+)
+
+var (
+	cfg config.Config
+	log *zap.SugaredLogger
 )
 
 func main() {
-	cfg := config.Load()
+	cfg = config.Load()
+	log = logger.New(cfg.Application)
 
-	log := logger.New(cfg.Application.Env)
-
-	db, err := database.New(cfg.Database)
+	db, err := database.NewPostgresConn(cfg.Database)
 	if err != nil {
 		log.Fatal("failed to connect to database", err)
 	}
 	defer db.Close()
-
 	log.Info("database connection established")
 
-	rdb, err := cache.New(cfg.Redis)
+	client, err := database.NewRedisClient(cfg.Redis)
 	if err != nil {
 		log.Fatal("failed to connect to redis", err)
 	}
-	defer rdb.Close()
-
+	defer client.Close()
+	cache := storage.NewCacheStorage(client)
 	log.Info("redis connection established")
 
-	log.Info("starting server")
+	tokenManager := auth.NewTokenManager(cfg.Application.Secret)
+	orchestrator := orchestrator.New(cfg, log, db, cache, tokenManager)
+	symphony := orchestrator.Compose()
+
+	router := router.NewServerRouter(cfg, symphony)
+
+	log.Fatal(run(router.Mount(client)))
+}
+
+func run(handler http.Handler) error {
+	srv := &http.Server{
+		Addr:         cfg.API.Addr(),
+		Handler:      handler,
+		WriteTimeout: cfg.API.WriteTimeout,
+		ReadTimeout:  cfg.API.ReadTimeout,
+		IdleTimeout:  cfg.API.IdleTimeout,
+	}
+
+	shutdown := make(chan error)
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		log.Infow("Signal caught", "signal", s.String())
+
+		shutdown <- srv.Shutdown(ctx)
+	}()
+
+	log.Infow("Server has started", "addr", cfg.API.Addr(), "env", cfg.Application.Env)
+
+	err := srv.ListenAndServe()
+	if err != http.ErrServerClosed {
+		return err
+	}
+
+	err = <-shutdown
+	if err != nil {
+		return err
+	}
+
+	log.Infow("Server has stopped", "addr", cfg.API.Addr(), "env", cfg.Application.Env)
+
+	return nil
 }
---
+++ cmd/server/router/router.go
@@ -0,0 +1,71 @@
+package router
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/cmd/server/orchestrator"
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/pkg/config"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/httprate"
+	httprateredis "github.com/go-chi/httprate-redis"
+	"github.com/redis/go-redis/v9"
+)
+
+type ServerRouter struct {
+	cfg      config.Config
+	symphony *orchestrator.Symphony
+}
+
+func NewServerRouter(cfg config.Config, symphony *orchestrator.Symphony) *ServerRouter {
+	return &ServerRouter{
+		cfg:      cfg,
+		symphony: symphony,
+	}
+}
+
+func (srv *ServerRouter) Mount(client *redis.Client) http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+	// Basic CORS
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   srv.cfg.API.CORS.AllowedOrigins,
+		AllowedMethods:   srv.cfg.API.CORS.AllowedMethods,
+		AllowedHeaders:   srv.cfg.API.CORS.AllowedHeaders,
+		ExposedHeaders:   srv.cfg.API.CORS.ExposedHeaders,
+		AllowCredentials: srv.cfg.API.CORS.AllowCredentials,
+		MaxAge:           srv.cfg.API.CORS.MaxAge, // Maximum value not ignored by any of major browsers
+	}))
+	// Rate Limiting: 10 Reqs/Min for each endpoint.
+	// IP stored in Redis with key httprate:[ipaddr]
+	r.Use(httprate.Limit(
+		srv.cfg.API.RateLimiter.RequestLimit,
+		srv.cfg.API.RateLimiter.WindowLength,
+		httprate.WithKeyFuncs(httprate.KeyByIP, httprate.KeyByEndpoint),
+		httprateredis.WithRedisLimitCounter(&httprateredis.Config{
+			Client: client,
+		}),
+	))
+
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Route("/auth", func(r chi.Router) {
+			srv.symphony.Auth.RegisterRoutes(r)
+		})
+
+		r.Route("/user", func(r chi.Router) {
+			srv.symphony.User.RegisterRoutes(r)
+		})
+	})
+
+	return r
+}
