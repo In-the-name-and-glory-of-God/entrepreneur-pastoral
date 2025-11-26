900e98a07101d97f87b9ab0260ca95c04b35d16c
Mr. Utzig <lutzig.octavius@gmail.com>
2025-11-25 23:19:22 -0300

    feat: Add services table migration

---
+++ pkg/database/migrations/000012_create_table_services.up.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS services (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    -- Foreign Key to the Business table
+    business_id UUID NOT NULL,
+
+    -- Service Details
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    
+    -- Price (Decimal type for accurate money handling)
+    price NUMERIC(10, 2) NOT NULL CHECK (price >= 0),
+    
+    -- Timestamps
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    -- Constraints
+    CONSTRAINT fk_business
+        FOREIGN KEY(business_id)
+        REFERENCES business(id)
+        ON DELETE RESTRICT -- Prevents deleting a business if services are still assigned to it
+        ON UPDATE CASCADE
+);
