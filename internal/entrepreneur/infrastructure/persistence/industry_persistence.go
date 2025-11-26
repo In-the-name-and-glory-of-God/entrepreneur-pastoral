0707e9686257526101c5b8efb724790c97e15aec
Mr. Utzig <lutzig.octavius@gmail.com>
2025-11-26 00:11:54 -0300

    feat: Add entrepreneur persistance data layer

---
+++ internal/entrepreneur/infrastructure/persistence/industry_persistence.go
@@ -0,0 +1,125 @@
+package persistence
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/In-the-name-and-glory-of-God/entrepreneur-pastoral/internal/entrepreneur/domain"
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jmoiron/sqlx"
+)
+
+type IndustryPersistence struct {
+	db   *sqlx.DB
+	psql sq.StatementBuilderType
+}
+
+func NewIndustryPersistence(db *sqlx.DB) *IndustryPersistence {
+	return &IndustryPersistence{
+		db:   db,
+		psql: sq.StatementBuilder.PlaceholderFormat(sq.Dollar),
+	}
+}
+
+func (r *IndustryPersistence) Create(ctx context.Context, industry *domain.Industry) error {
+	query, args, err := r.psql.Insert("industries").
+		Columns("name").
+		Values(industry.Name).
+		Suffix("RETURNING id").
+		ToSql()
+
+	if err != nil {
+		return fmt.Errorf("failed to build create industry query: %w", err)
+	}
+
+	if err := r.db.QueryRowxContext(ctx, query, args...).Scan(&industry.ID); err != nil {
+		return fmt.Errorf("failed to execute create industry query: %w", err)
+	}
+
+	return nil
+}
+
+func (r *IndustryPersistence) Update(ctx context.Context, industry *domain.Industry) error {
+	query, args, err := r.psql.Update("industries").
+		Set("name", industry.Name).
+		Where(sq.Eq{"id": industry.ID}).
+		ToSql()
+
+	if err != nil {
+		return fmt.Errorf("failed to build update industry query: %w", err)
+	}
+
+	if _, err := r.db.ExecContext(ctx, query, args...); err != nil {
+		return fmt.Errorf("failed to execute update industry query: %w", err)
+	}
+
+	return nil
+}
+
+func (r *IndustryPersistence) Delete(ctx context.Context, id int16) error {
+	query, args, err := r.psql.Delete("industries").
+		Where(sq.Eq{"id": id}).
+		ToSql()
+
+	if err != nil {
+		return fmt.Errorf("failed to build delete industry query: %w", err)
+	}
+
+	if _, err := r.db.ExecContext(ctx, query, args...); err != nil {
+		return fmt.Errorf("failed to execute delete industry query: %w", err)
+	}
+
+	return nil
+}
+
+func (r *IndustryPersistence) GetAll(ctx context.Context) ([]*domain.Industry, error) {
+	var industries []*domain.Industry
+	query, args, err := r.psql.Select("*").From("industries").OrderBy("name ASC").ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build get all industries query: %w", err)
+	}
+
+	if err := r.db.SelectContext(ctx, &industries, query, args...); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, domain.ErrIndustryNotFound
+		}
+		return nil, fmt.Errorf("failed to execute get all industries query: %w", err)
+	}
+
+	return industries, nil
+}
+
+func (r *IndustryPersistence) GetByID(ctx context.Context, id int16) (*domain.Industry, error) {
+	var industry domain.Industry
+	query, args, err := r.psql.Select("*").From("industries").Where(sq.Eq{"id": id}).Limit(1).ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build get industry by id query: %w", err)
+	}
+
+	if err := r.db.GetContext(ctx, &industry, query, args...); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, domain.ErrIndustryNotFound
+		}
+		return nil, fmt.Errorf("failed to execute get industry by id query: %w", err)
+	}
+
+	return &industry, nil
+}
+
+func (r *IndustryPersistence) GetByName(ctx context.Context, name string) (*domain.Industry, error) {
+	var industry domain.Industry
+	query, args, err := r.psql.Select("*").From("industries").Where(sq.Eq{"name": name}).Limit(1).ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build get industry by name query: %w", err)
+	}
+
+	if err := r.db.GetContext(ctx, &industry, query, args...); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, domain.ErrIndustryNotFound
+		}
+		return nil, fmt.Errorf("failed to execute get industry by name query: %w", err)
+	}
+
+	return &industry, nil
+}
